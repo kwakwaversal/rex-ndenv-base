f7add1badd9c3526af3a56c473f378c057821ed8
Paul Williams <kwakwaversal@gmail.com>
2016-10-19 14:53:16 +0100

    Replace plenv commands with ndenv equivalents.

--- __module__.pm
+++ __module__.pm
@@ -1,9 +1,9 @@
 #
 # AUTHOR: Paul Williams <kwakwa@cpan.org>
-# REQUIRES: build-essential, curl, git
+# REQUIRES: build-essential, git
 # LICENSE: Apache License 2.0
 #
-# A Rex module to install ndenv and node on your Server.
+# A Rex module to install ndenv and build node on your Server.
 
 package Rex::Ndenv::Base;
 
@@ -19,73 +19,58 @@ our %version_map = (
 
 # The prepare task needs root privileges. Run as root.
 task prepare => make {
-  pkg [qw/build-essential curl git/], ensure => "latest";
+  pkg [qw/build-essential git/], ensure => "latest";
 };
 
 task setup => make {
-  my $perl_version = param_lookup "perl_version",
+  my $node_version = param_lookup "node_version",
     $version_map{ lc get_operating_system };
 
   # Commands taken from
   #
-  # http://weblog.bulknews.net/post/58079418600/plenv-alternative-for-perlbrew
-  # https://github.com/tokuhirom/plenv#readme
+  # https://github.com/riywo/ndenv
 
-  # Check out plenv into ~/.plenv
-  run 'ls ~/.plenv';
+  # Check out ndenv into ~/.ndenv
+  run 'ls ~/.ndenv';
   if ($? == 0) {
-    Rex::Logger::info "plenv has already been installed";
+    Rex::Logger::info "ndenv has already been installed";
   }
   else {
-    run 'git clone git://github.com/tokuhirom/plenv.git ~/.plenv';
+    run 'git clone git://github.com/riywo/ndenv.git ~/.ndenv';
   }
 
-  # Add ~/.plenv/bin to your $PATH for access to the plenv command-line utility.
-  # Add plenv init to your shell to enable shims and autocompletion.
+  # Add ~/.ndenv/bin to your $PATH for access to the ndenv command-line utility.
+  # Add ndenv init to your shell to enable shims and autocompletion.
   run 'touch ~/.bash_profile';
-  append_if_no_such_line '~/.bash_profile', 'export PATH="$HOME/.plenv/bin:$PATH"';
-  append_if_no_such_line '~/.bash_profile', 'eval "$(plenv init -)"';
+  append_if_no_such_line '~/.bash_profile', 'export PATH="$HOME/.ndenv/bin:$PATH"';
+  append_if_no_such_line '~/.bash_profile', 'eval "$(ndenv init -)"';
 
-  # Install perl-build, which provides a plenv install command that simplifies
-  # the process of installing new Perl versions.
-  run 'ls ~/.plenv/plugins/perl-build/';
+  # Install node-build, which provides a ndenv install command that simplifies
+  # the process of installing new Node versions.
+  run 'ls ~/.ndenv/plugins/node-build/';
   if ($? == 0) {
-    Rex::Logger::info "perl-build has already been installed";
+    Rex::Logger::info "node-build has already been installed";
   }
   else {
-    run 'git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/';
+    run 'git clone git://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build/';
   }
 
-  # Additional commands to make plenv more perlbrew-like
-  # run 'git clone git://github.com/miyagawa/plenv-contrib.git ~/.plenv/plugins/plenv-contrib/';
+  # Additional commands to make ndenv more perlbrew-like
+  # run 'git clone git://github.com/miyagawa/ndenv-contrib.git ~/.ndenv/plugins/ndenv-contrib/';
 
-  # Add ~/plenv/bin to the Rex $PATH so it will be used for plenv commands
+  # Add ~/ndenv/bin to the Rex $PATH so it will be used for ndenv commands
   my @new_path;
-  push(@new_path, '$HOME/.plenv/bin');
+  push(@new_path, '$HOME/.ndenv/bin');
   push(@new_path, Rex::Config->get_path);
   Rex::Config->set_path(\@new_path);
 
-  # Install perl!
-  run "plenv versions |grep $perl_version";    # this is probably a bug
+  # Install node!
+  run "ndenv versions |grep $node_version";    # this is probably a bug
   if ($? == 0) {
-    Rex::Logger::info "perl-$perl_version has already been installed";
+    Rex::Logger::info "perl-$node_version has already been installed";
   }
   else {
-    run "plenv install $perl_version";
-  }
-
-  # Because all perl-based scripts (prove, plackup etc.) are also wrapped as
-  # shims, you have to run plenv rehash after installing CPAN modules that have
-  # scripts. But if you run plenv install-cpanm to bootstrap cpanminus, it will
-  # be automatically taken care of.
-  run "plenv rehash";
-
-  run "PLENV_VERSION=$perl_version plenv which cpanm";
-  if ($? == 0) {
-    Rex::Logger::info "cpanm has already installed for perl-$perl_version";
-  }
-  else {
-    run "PLENV_VERSION=$perl_version plenv install-cpanm";
+    run "ndenv install $node_version";
   }
 };
 
